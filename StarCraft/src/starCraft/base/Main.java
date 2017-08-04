155496bb3a68c619370d5093c88cddd3871a340e
Eis <faivre.chloe@gmail.com>
2017-08-04 10:46:27 +0200

    First Commit
    Creation classes Main and Marine

---
+++ StarCraft/src/starCraft/base/Main.java
@@ -0,0 +1,11 @@
+package starCraft.base;
+
+public class Main {
+
+	public static void main(String[] args){
+	    String [] nom = new String[1];
+	    nom[0] = "RobinLaMouette";
+	    
+	    System.out.println("Bienvenue dans chlocraft : " + nom[0] );
+	}
+}
---
+++ StarCraft/src/starCraft/base/Marine.java
@@ -0,0 +1,9 @@
+package starCraft.base;
+
+public class Marine {
+
+	public int damage;
+	public int attaqueSpeed;
+	public int healthPoint;
+
+}
