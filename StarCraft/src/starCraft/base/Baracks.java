fd568943c0f5d70ffbf3dbb21e115b126e1bb1a8
Eis <faivre.chloe@gmail.com>
2017-08-07 11:57:47 +0200

    baracks change

--- StarCraft/src/starCraft/base/Baracks.java
+++ StarCraft/src/starCraft/base/Baracks.java
@@ -1,10 +1,20 @@
 package starCraft.base;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Baracks {
-	public void train(ArrayList list){
-		this.getName()
+	
+	public void train(List<String> listNom){
+		List<String> listeNom = new ArrayList<String>();
+		
+		//ajoute nom du Marine
+		listeNom.add("");
+		
+		for(int i = 0; i < listeNom.size(); i++){
+			System.out.println("Creation du Marine " + listeNom.get(i));
+		}
+		
 	}
 
 }
