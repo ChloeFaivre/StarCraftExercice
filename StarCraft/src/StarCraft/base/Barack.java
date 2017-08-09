6b433648fc54a51e6cbcaa33bd36c62bf8b66e2f
Eis <faivre.chloe@gmail.com>
2017-08-09 14:25:23 +0200

    Create barack

--- StarCraft/src/StarCraft/base/Barack.java
+++ StarCraft/src/StarCraft/base/Barack.java
@@ -5,16 +5,17 @@ import java.util.List;
 
 public class Barack {
 	
-	/*public List<Marine> train(List<String> namesMarine){
+	public List<Marine> train(List<String> namesMarine){
 		List<Marine> marines = new ArrayList<>();
 		for(String name : namesMarine){
-			marines.add(new Marine(name));
+			Marine myMarine = new Marine(name);
+			marines.add(myMarine);
 		}
 		return marines;	
-	}*/
+	}
 	
-	public Barack train(String namesMarine){
-		Barack myMarine = new Barack();
+	public Marine train(String namesMarine){
+		Marine myMarine = new Marine(namesMarine);
 		
 		return myMarine;
 	}
--- StarCraft/src/StarCraft/base/Main.java
+++ StarCraft/src/StarCraft/base/Main.java
@@ -1,18 +1,28 @@
 package src.StarCraft.base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args){
 		System.out.println("Bienvenue dans myStarcraft : " + args[0]);
 
-	    /*Marine gibbs = new Marine("Gibbs");
-	    Marine raynor = new Marine("Raynor");
-	    
-	    gibbs.attack(raynor);
-		while (raynor.getHealthPoint() > 0) {
-			gibbs.attack(raynor);
-		}*/
+	    /*Marine gibbs = new Marine("Gibbs");*/
+		Barack barack = new Barack();
 		
-		Barack raynor = new Barack();
+		List<String> namesMarine = new ArrayList<>();		
+		namesMarine.add("Raynor");
+		namesMarine.add("Jim");
+		namesMarine.add("Tychus");
+		namesMarine.add("Cortana");
+				
+		List<Marine> marines = barack.train(namesMarine);
+		Marine martin = barack.train("Martin");
+		
+//	    gibbs.attack(raynor);
+//		while (raynor.getHealthPoint() > 0) {
+//			gibbs.attack(raynor);
+//		}
 	}
 }
