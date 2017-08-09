343c99e152950f0071c52828edb5216602d5dc04
Eis <faivre.chloe@gmail.com>
2017-08-09 11:40:27 +0200

    Begin

--- StarCraft/src/StarCraft/base/Barack.java
+++ StarCraft/src/StarCraft/base/Barack.java
@@ -1,16 +1,21 @@
-package StarCraft.base;
+package src.StarCraft.base;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Barack {
 	
-	public List<Marine> train(List<String> namesMarine){
+	/*public List<Marine> train(List<String> namesMarine){
 		List<Marine> marines = new ArrayList<>();
 		for(String name : namesMarine){
 			marines.add(new Marine(name));
 		}
 		return marines;	
-	}
+	}*/
 	
+	public Barack train(String namesMarine){
+		Marine myMarine = new Marine(name);
+		
+		return myMarine;
+	}
 }
