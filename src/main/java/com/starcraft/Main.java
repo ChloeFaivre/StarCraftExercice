634bfbd16703df631f2fe9c803c1120e9d6452d2
Eis <faivre.chloe@gmail.com>
2017-08-11 17:34:52 +0200

    Final? Non

--- src/main/java/com/starcraft/Main.java
+++ src/main/java/com/starcraft/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Main {
 
@@ -14,59 +15,29 @@ public class Main {
 		Barack barack = new Barack();
 		
 		List<String> namesMarine = new ArrayList<>();	
-		
 		namesMarine.add("Raynor");
 		namesMarine.add("Jim");
 		namesMarine.add("Tychus");
 		namesMarine.add("Cortana");
-				
-		Map<Integer, Marine> marines = new HashMap<>();
-		//Map<Integer, Marine> marines = new HashMap<Integer, barack.train(namesMarine)>();
-		//marines.put(key, barack.train(namesMarine));
-		//marines.put(Index, namesMarine);
-		Marine martin = barack.train("Martin");
-		//attaquer liste Marine 
-		marines.put(1, martin);
+		namesMarine.add("Martin");
 		
+		Map<String, Marine> marines = new HashMap<>();
 		
-		namesMarine[10];// ==> ceci est un tableau d'une taille 10 c'est pas bien on fait des list
-
-		for (i = 0; i < 10; i++) {
-			Marine marine = New Marine(nameList[i]);
-			myMap.put(nameList[i], marine);
+		for (Integer i = 0; i < namesMarine.size(); i++) {
+			Marine marine = new Marine(namesMarine.get(i));
+			marines.put(namesMarine.get(i), marine);
 		}
 		
+		String champion = "Raynor";
 		
-		
-		//Tout d'abord j'instancie une map qui va prendre comme cle (==key, ==k) une string (== nom du marine) et comme valeur un objet de type marine
-		//Ensuite comme j'ai une liste avec tout les noms des marines, je vais boucler sur tous ces marines.
-		//pour chacun de ses nom de marines je vais :
-		// 			créer un marine
-		// 			et l'ajouter à ma map (avec pour clé le nom du marine et pour valeur le marine nouvellement créer)
-		
-		
-		
-		
-		//List<Marine> marines = barack.train(namesMarine);
-		System.out.println(marines);
-		//System.out.println(marines.get(1));
-		//Revoir le tout pour modifier en map
-		for(int i = 0; i< marines.size() ; i++){
-			if(marines.get(i) == marines.get(1)){
-				continue;
+		for (Entry<String, Marine> entry : marines.entrySet()) {
+			String myName = entry.getKey();
+			Marine myMarine = entry.getValue();
+			if (myName != champion) {
+				while (myMarine.getHealthPoint() > 0){
+					marines.get(champion).attack(myMarine);
 				}
-			marines.get(1).attack(marines.get(i));
-			while (marines.get(i).getHealthPoint() > 0){
-				marines.get(1).attack(marines.get(i));
 			}
-		}
-		
-		//une solution ou raynor est le champion
-		for(int i = 1; i< marines.size() ; i++){
-			while (marines.get(i).getHealthPoint() > 0){
-				marines.get(0).attack(marines.get(i));
-			}
-		}
-		
+		}		
 	}
 }
